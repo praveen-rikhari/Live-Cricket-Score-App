c26d2c8961cb4ccbc46e9bb879901e1cd46a84f0
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-23 23:34:05 +0530

    created Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-logo">
+                    <img src="/public/cricket-ball.webp" alt="logo" />
+                    <span>CricMate</span>
+                </div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink to="/" className="nav-link">
+                            Live Score
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/series" className="nav-link">
+                            Series
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/matches" className="nav-link">
+                            Matches
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/players" className="nav-link">
+                            Players
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <hr />
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;
