ae5e14fce4372597d37e21d033b353f18c5b0861
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-23 23:35:17 +0530

    Fixed minor bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.jsx';
+import LiveScore from './components/LiveScore/LiveScore.jsx';
+import Series from './components/Series/Series.jsx';
+import Matches from './components/Matches/Matches.jsx';
+import Players from './components/Players/Players.jsx';
+import './App.css'
 
 function App() {
   return (
-    <div>Live score Cricket App</div>
-  )
+
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={LiveScore} />
+        <Route path="/series" component={Series} />
+        <Route path="/matches" component={Matches} />
+        <Route path="/players" component={Players} />
+      </Routes>
+    </div>
+
+  );
 }
 
-export default App
+export default App;
